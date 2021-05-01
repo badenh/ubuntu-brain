3862e01ce441e2dd144bc5c2afc5f5c68f092e28
Baden Hughes <580499+badenh@users.noreply.github.com>
2021-05-01 12:43:40 +1000

    Update scrape_packages.sh

--- scrape_packages.sh
+++ scrape_packages.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 # script that scrapes package list from NeuroFedora and COPR to bootstrap the software instalaltion
+
+wget https://pagure.io/neuro-sig/documentation/blob/main/f/modules/ROOT/pages/compneuro-tools.adoc
+wget https://pagure.io/neuro-sig/documentation/blob/main/f/modules/ROOT/pages/copr.adoc
