81795d3d3bdeb8101eead8f81a02298446a3a843
Baden Hughes <580499+badenh@users.noreply.github.com>
2021-05-03 15:34:55 +1000

    Update find_packages.sh

--- find_packages.sh
+++ find_packages.sh
@@ -4,7 +4,12 @@
 # basically get the full list of fedora packages, pick out ones that match neurofedora, then get them one at a time
 
 # badenh@ubuntu:~$ wget https://download-ib01.fedoraproject.org/pub/fedora/linux/releases/33/Everything/x86_64/os/Packages/n/
-# badenh@ubuntu:~$ cat index.html | grep neurord | cut -d \" -f 6
-# neurord-3.2.2-12.fc33.noarch.rpm
-# neurord-javadoc-3.2.2-12.fc33.noarch.rpm
+ls index.html.* | while read i; do cat $i >> allindex; done
+
+cat packagelist | while read i; do grep -i allindex >> extractindex; done
+
+cat extractindex | cut -d \" -f 6 >> rpmlist
+
+cat rpmlist | while read i; do wget ..
+
 # badenh@ubuntu:~$ cat index.html | grep neurord | cut -d \" -f 6 | while read i; do wget https://download-ib01.fedoraproject.org/pub/fedora/linux/releases/33/Everything/aarch64/os/Packages/n/$i; done
