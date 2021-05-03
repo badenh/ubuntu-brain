f20934edf642cab7c1dd2f7a3ec98ef464c7a7ec
Baden Hughes <580499+badenh@users.noreply.github.com>
2021-05-03 14:29:35 +1000

    Update scrape_packages.sh

--- scrape_packages.sh
+++ scrape_packages.sh
@@ -4,5 +4,6 @@
 wget https://pagure.io/neuro-sig/documentation/blob/main/f/modules/ROOT/pages/compneuro-tools.adoc
 wget https://pagure.io/neuro-sig/documentation/blob/main/f/modules/ROOT/pages/copr.adoc
 
-cat compneuro-tools.adoc | grep sudo | cut -d \` -f 2 > packagelist
-cat copr.adoc | grep sudo | cut -d \` -f 2 >> packagelist
+cat compneuro-tools.adoc | grep sudo | grep install | cut -d \` -f 2 > packagelist
+cat copr.adoc | grep sudo | grep install | cut -d \` -f 2 >> packagelist
+cat packagelist | sed s/dnf/apt-get/g > packagelist
